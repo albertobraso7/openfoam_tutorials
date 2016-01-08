1a9869b21506615da3c45cdeaf621a72097ee504
Robert Lee <rlee32@gatech.edu>
2016-01-08 12:32:36 -0500

    compressed repo

--- TOOLBOX/case_cleanup_scripts/README.md
+++
@@ -1,4 +0,0 @@
-This repository has various scripts to compact parallel (decomposed) and serial 
-openfoam cases.
-
-TODO: provide description of scripts.
--- TOOLBOX/case_cleanup_scripts/mass_cleanseq.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for folder in */ ; do
-    if [ -d "${folder}system" ]; then
-      echo "Cleaning $folder"
-      ~/Desktop/cleanseq.py $folder
-    fi
-done
